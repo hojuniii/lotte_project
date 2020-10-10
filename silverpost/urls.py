f1d2e80fa7111a67c9b72e3472c45d585247c3e1
tkddbs0411 <tkddbs0411@gmail.com>
2020-10-10 19:53:11 +0900

    image_to_server

--- silverpost/urls.py
+++ silverpost/urls.py
@@ -1,9 +1,11 @@
 # mysite/urls.py
 from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("api/auth", include("knox.urls")),
-]
+] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
